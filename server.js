03d6f0afa2cd572f21ccfe69152ed5d1b67ae840
Rohit Kumar <rohit_kumarr@outlook.com>
2020-12-28 03:15:39 +0530

    signin route edit 3

--- server.js
+++ server.js
@@ -3,39 +3,39 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const database = [
-    {
-        id: 123,
-        name: "Rohit",
-        email: "rohit@gmail.com",
-        password: "cookies",
-        entries: 0,
-        joined: new Date()
-    },
-    {
-        id: 143,
-        name: "Ankit",
-        email: "ankit@gmail.com",
-        password: "bananas",
-        entries: 0,
-        joined: new Date()
-    }
+  {
+    id: 123,
+    name: "Rohit",
+    email: "rohit@gmail.com",
+    password: "cookies",
+    entries: 0,
+    joined: new Date(),
+  },
+  {
+    id: 143,
+    name: "Ankit",
+    email: "ankit@gmail.com",
+    password: "bananas",
+    entries: 0,
+    joined: new Date(),
+  },
 ];
 
-app.get("/", (req,res)=>{
-    res.send("Hello from the backend!");
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
 });
-""
-app.post("/signin",(req,res)=>{
-
-    if(req.body.email === database[0].email && req.body.password === database[0].password){
-        res.send("success!");
-    }
-else{
-    res.send("error!");
-}
-
+("");
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database[0].email &&
+    req.body.password === database[0].password
+  ) {
+    res.send("success!");
+  } else {
+    res.status(404).json("error!");
+  }
 });
 
-app.listen(3000, ()=>{
-    console.log("Server at 3000 is running.");
-})
+app.listen(3000, () => {
+  console.log("Server at 3000 is running.");
+});
