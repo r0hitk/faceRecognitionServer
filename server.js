31a53fc18c9dec63d3b8c1d014a98b49369eac04
Rohit Kumar <rohit_kumarr@outlook.com>
2020-12-28 03:05:20 +0530

    signin route

--- server.js
+++ server.js
@@ -2,8 +2,31 @@ const express = require("express");
 
 const app = express();
 
+const database = [
+    {
+        id: 123,
+        name: "Rohit",
+        email: "rohit@gmail.com",
+        password: "cookies",
+        entries: 0,
+        joined: new Date()
+    },
+    {
+        id: 143,
+        name: "Ankit",
+        email: "ankit@gmail.com",
+        password: "bananas",
+        entries: 0,
+        joined: new Date()
+    }
+];
+
 app.get("/", (req,res)=>{
     res.send("Hello from the backend!");
+});
+""
+app.post("/signin",(req,res)=>{
+res.send("signing!");
 });
 
 app.listen(3000, ()=>{
