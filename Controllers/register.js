095b41af3e668e6c7aea14f3da310e719243d308
Rohit Kumar <rohit_kumarr@outlook.com>
2021-01-10 16:21:53 +0530

    debug

--- Controllers/register.js
+++ Controllers/register.js
@@ -30,7 +30,7 @@ const handleRegister = (req, res, knex, bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback);
         })
-        .catch((err) => res.status(400).json(err));
+        .catch((err) => res.status(400).json("here i am"));
     });
   } else {
     res.status(400).json("Unable to register");
